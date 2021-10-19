8e6ff3958f6ff750cbf5711ca0ab61eafddcd8f0
citlali-trigos-raczkowski <cbtrigos@gmail.com>
2021-10-18 22:34:14 -0700

    feat(detailed-page): creating pokemon view

--- frontend/src/views/pokemon-detail.vue
+++ frontend/src/views/pokemon-detail.vue
@@ -1,41 +1,44 @@
 <template>
-  <div id="body">
-    <router-link to="/">Return home</router-link>
+  <div>
     <loader v-if="loading" />
-    <div v-else>
-      <div class="pokemonDetail">
-        <h1>Pokemon Detail</h1>
-        name {{ pokemon.name }} <br />
+    <div v-else id="pokemon-detail">
+      <div id="pokemon-detail-left">
         <img
-          id="pokemon-image"
+          id="pokemon-detail-image"
           v-bind:src="pokemon.image"
           alt="pokemon-image"
         />
-        <br />types: [
-        <div v-for="type in pokemon.types" v-bind:key="type">
-          {{ type }}
+      </div>
+      <div id="pokemon-detail-info">
+        <div id="tile-heart">
+          <heart-toggle
+            :togglePokemonfavorite="togglePokemonfavorite"
+            :heartColor="heartColor"
+          />
         </div>
-        ] <br />isFavorite: {{ pokemon.isFavorite }} <br />
+        <h1 id="pokemon-detail-name">{{ pokemon.name }}</h1>
+        <br />
+        <strong>Types:</strong> {{ pokemon.types.join(", ") }}
+
         <div id="pokemon-weight" v-if="pokemon && pokemon.weight">
-          weight: {{ pokemon.weight.minimum }} - {{ pokemon.weight.maximum
-          }}<br />
+          <strong>Weight</strong>: {{ pokemon.weight.minimum }} -
+          {{ pokemon.weight.maximum }}<br />
         </div>
         <div id="pokemon-height" v-if="pokemon && pokemon.height">
-          weight: {{ pokemon.height.minimum }} - {{ pokemon.height.maximum
-          }}<br />
+          <strong>Height</strong>: {{ pokemon.height.minimum }} -
+          {{ pokemon.height.maximum }}<br />
         </div>
-
-        Max CP: {{ pokemon.maxCP }}<br />
-        Max HP: {{ pokemon.maxHP }}<br />
-        Evolutions: [
-        <pokemon-evolution-tile
-          v-for="pokemon in pokemon.evolutions"
-          :key="pokemon.id"
-          :pokemon="pokemon"
-        >
-        </pokemon-evolution-tile>
-        ]
+        <strong>Max CP</strong>: {{ pokemon.maxCP }}<br />
+        <strong>Max HP</strong>: {{ pokemon.maxHP }}<br />
       </div>
+    </div>
+    <div id="evolution-tiles">
+      <pokemon-evolution-tile
+        v-for="pokemon in pokemon.evolutions"
+        :key="pokemon.id"
+        :pokemon="pokemon"
+      >
+      </pokemon-evolution-tile>
     </div>
   </div>
 </template>
@@ -49,10 +52,20 @@ import { logGenericError } from "../helper-functions/logging";
 import { getPathname, reRouteto } from "../helper-functions/routes";
 import { DetailedPokemonType } from "../types/pokemon-types";
 import Loader from "../components/loader.vue";
+import HeartToggle from "../components/heart-toggle.vue";
+import { getHeartColor } from "../helper-functions/get-heart-color";
+import {
+  FavoritePokemon,
+  UnFavoritePokemon,
+} from "../api/update-favorite-status";
 
 export default Vue.extend({
   name: "PokemonDetail",
-  components: { "pokemon-evolution-tile": EvolutionTile, loader: Loader },
+  components: {
+    "pokemon-evolution-tile": EvolutionTile,
+    loader: Loader,
+    "heart-toggle": HeartToggle,
+  },
   watch: {
     $route(to, from) {
       if (to !== from) {
@@ -70,6 +83,7 @@ export default Vue.extend({
           if (response) {
             Object.assign({}, this.pokemon, { a: 1, b: 2 });
             this.pokemon = response as DetailedPokemonType;
+            this.reloadHeart();
             this.loading = false;
             return;
           }
@@ -90,15 +104,51 @@ export default Vue.extend({
     pokemonNotEmpty: function (): boolean {
       return Object.keys(this.pokemon).length === 0;
     },
+    reloadHeart: function () {
+      this.heartColor = getHeartColor(this.pokemon);
+    },
+    togglePokemonfavorite: function () {
+      if (this.pokemon.isFavorite) {
+        UnFavoritePokemon({ pokemonId: this.pokemon.id }).then((response) => {
+          if (
+            response &&
+            response.unFavoritePokemon &&
+            response.unFavoritePokemon.id === this.pokemon.id
+          ) {
+            console.log("successfully unfavorited");
+            this.pokemon.isFavorite = false;
+            this.reloadHeart();
+          } else {
+            console.log("failed to unfavorite");
+          }
+        });
+      } else {
+        FavoritePokemon({ pokemonId: this.pokemon.id }).then((response) => {
+          if (
+            response &&
+            response.favoritePokemon &&
+            response.favoritePokemon.id === this.pokemon.id
+          ) {
+            console.log("successfully favorited");
+            this.pokemon.isFavorite = true;
+            this.reloadHeart();
+          } else {
+            console.log("failed to favorite");
+          }
+        });
+      }
+    },
   },
   beforeMount() {
     this.loadPokemon(this.windowLocation);
   },
   data: function () {
+    const pokemon: DetailedPokemonType = {} as DetailedPokemonType;
     return {
       loading: true,
       windowLocation: RemoveSlashes(getPathname()),
-      pokemon: {},
+      pokemon,
+      heartColor: "",
     };
   },
 });
