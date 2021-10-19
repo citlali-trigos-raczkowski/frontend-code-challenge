7ff0159be4d511c1eee93d4cabf65c63c24de14a
citlali-trigos-raczkowski <cbtrigos@gmail.com>
2021-10-18 20:52:32 -0700

    feat(footer): locked at bottom

--- frontend/src/assets/constants.ts
+++ frontend/src/assets/constants.ts
@@ -1,6 +1,13 @@
-export const PageTitle = "Who's That Pokemon?"
-export const SearchPlaceholder = "Search for your favorite Pokemon..."
-export const DropdownPlaceholder = "Filter by type"
+export const PageTitle = "Who's That Pokemon?";
+export const SearchPlaceholder = "Search for your favorite Pokemon...";
+export const DropdownPlaceholder = "Filter by type";
+const NoFilterDropdownOption = "No Filter";
+export const NothingDropdownOption = {
+  text: NoFilterDropdownOption,
+  value: "all",
+};
 
-const NoFilterDropdownOption = "No Filter"
-export const NothingDropdownOption = { text: NoFilterDropdownOption, value: "all" };
+export const DeveloperName = "Citlali Trigos-Raczkowski";
+export const LinkedInLink =
+  "https://www.linkedin.com/in/citlali-trigos-raczkowski/";
+export const GithubLink = "https://github.com/citlali-trigos-raczkowski";
--- frontend/src/components/footer.vue
+++ frontend/src/components/footer.vue
@@ -1,51 +1,42 @@
 <template>
-  <v-footer
-    dark
-    padless
-  >
-    <v-card
-      class="flex"
-      flat
-      tile
-    >
-      <v-card-title class="grey">
-        <strong class="subheading">Thanks for visiting!</strong>
-
-        <v-spacer></v-spacer>
-
-        <v-btn
-          v-for="icon in icons"
-          :key="icon"
-          class="mx-4"
-          dark
-          icon
-        >
-          <v-icon size="24px">
-            {{ icon }}
-          </v-icon>
+  <v-footer dark id="footer">
+    <!-- <v-card  class="flex" flat tile> -->
+    <v-card-title id="footer-top">
+      <div id="footer-left"/>
+      <div id="footer-center">
+        <strong id="footer-heading">Thanks for visiting!</strong>
+      </div>
+      <div id="footer-right">
+        <v-btn v-for="icon in icons" :key="icon.name" class="mx-4" dark icon>
+          <a target="_blank" :href="icon.link">
+            <v-icon color="black" size="24px">
+              {{ icon.name }}
+            </v-icon>
+          </a>
         </v-btn>
-      </v-card-title>
+      </div>
+    </v-card-title>
 
-      <v-card-text class="py-2 white--text text-center">
-        {{ new Date().getFullYear() }} — <strong>Vuetify</strong>
-      </v-card-text>
-    </v-card>
+    <v-card-text id="footer-bottom" class="py-2 white--text text-center">
+      {{ new Date().getFullYear() }} — {{ DeveloperName }}
+    </v-card-text>
+    <!-- </v-card> -->
   </v-footer>
 </template>
 
 <script lang="ts">
 import Vue from "vue";
+import { DeveloperName, LinkedInLink, GithubLink } from "../assets/constants";
 
 export default Vue.extend({
   name: "Footer",
   data() {
     return {
-       icons: [
-        'mdi-facebook',
-        'mdi-twitter',
-        'mdi-linkedin',
-        'mdi-instagram',
-      ]
+      icons: [
+        { name: "mdi-linkedin", link: LinkedInLink },
+        { name: "mdi-github", link: GithubLink },
+      ],
+      DeveloperName,
     };
   },
 });
